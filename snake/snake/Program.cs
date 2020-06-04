5a4ae6c13dc028ce0bf88a97c82c6f75b66fa61f
Anatoliy <Shugarovt@gmail.com>
2020-06-04 16:56:28 +0700

    Добавили поедание

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -26,14 +26,28 @@ namespace snake
             Snake snake = new Snake(p, 4,Direction.RIGHT);
             snake.Draw();
 
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             while (true) 
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
                 if (Console.KeyAvailable) 
                 {
                     ConsoleKeyInfo key = Console.ReadKey(true);
                     snake.HandleKey(key.Key);
                 }
-                Thread.Sleep(100);
+                Thread.Sleep(200);
                 snake.Move();
             }
         }
