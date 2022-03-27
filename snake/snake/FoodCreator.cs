23d05c5e622441c64153cbe7489b85c4e93069ac
StShu <shugarovt@gmail.com>
2022-03-27 12:15:21 +0700

    Переименовал переменные положения "еды для змейки"

--- snake/snake/FoodCreator.cs
+++ snake/snake/FoodCreator.cs
@@ -4,23 +4,23 @@ namespace snake
 {
 	class FoodCreator
 	{
-		int mapWidht;
-		int mapHeight;
+		int x;
+		int y;
 		char sym;
 
 		Random random = new Random();
 
-		public FoodCreator(int mapWidth, int mapHeight, char sym)
+		public FoodCreator(int x, int y, char sym)
 		{
-			this.mapWidht = mapWidth;
-			this.mapHeight = mapHeight;
+			this.x = x;
+			this.y = y;
 			this.sym = sym;
 		}
 
 		public Point CreateFood()
 		{
-			int x = random.Next(2, mapWidht - 2);
-			int y = random.Next(2, mapHeight - 2);
+			int x = random.Next(2, this.x - 2);
+			int y = random.Next(2, this.y - 2);
 			return new Point(x, y, sym);
 		}
 	}
